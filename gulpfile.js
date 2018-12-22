56855949424cf48c9830207a2c1786f0b1d56755
Oleg Frey <olfrey@ya.ru>
2018-12-22 03:05:33 +0300

    full

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,10 @@ gulp.task('scripts', function() {
 	return gulp.src([ 
 		'src/template/libs/jquery/dist/jquery.js',
 		'src/template/libs/jquery-mask-plugin/dist/jquery.mask.js',
-		'src/template/libs/fancybox/dist/jquery.fancybox.js'
+		'src/template/libs/fancybox/dist/jquery.fancybox.js',
+		'src/template/libs/slick-carousel/slick/slick.js',
+		'src/template/libs/jqvmap/js/jquery.vmap.js',
+		'src/template/libs/jqvmap/js/maps/jquery.vmap.russia.js'
 		])
 		.pipe(concat('libs.js')) 
 		.pipe(gulp.dest('src/template/scripts')); 
--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,42 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+
+	$('.slider__list').slick({
+		dots: true,
+		appendArrows: $('.slider__arrows'),
+		appendDots: $('.slider__dots')
+	})
+
+	$('.nav-block a[href = \\#]').click(function(event) {
+		event.preventDefault();
+		var ul = $(this).next('ul'),
+				li = $(this).parent('li');
+		ul.slideToggle();
+		li.toggleClass('toggle');
+	});
+
+
+	$('#popup-callback .btn').click(function(event) {
+		$.fancybox.close();
+		
+		$.fancybox.open({src  : '#popup-thanks',type : 'inline'});
+		
+	});
+
+
+	var hum = $('.hum'),
+			nav = $('.nav__list');
+
+	hum.click(function(event) {
+		hum.toggleClass('hum_toggle');
+		nav.slideToggle(300);
+	});
+
+	$(window).on('load resize', function() {
+		if ($(window).width() > 767) {
+			nav.removeAttr('style');
+		}
+		
+	});
 });
